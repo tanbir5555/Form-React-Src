4e194f3cc3023b745e11cf4fb2d594d4b7bf7a5b
tanbir5555 <tanbirhawlader12690@gmail.com>
2021-05-18 02:13:38 +0600

    FIRSET

---
+++ components/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './app.css'
+import Sign from './SignUp/Sign'
+// import {Grid} from '@material-ui/core'
+export default function App() {
+   
+    return (
+      <div className='container'>  
+      
+      <Sign/>
+     
+      
+       </div>
+    )
+}
